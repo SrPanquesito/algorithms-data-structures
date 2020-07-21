91eb4fb52c3ff72de865ff1b75b4ec12cd8e92a2
Luis Diaz <luisgdiazreynoso@gmail.com>
2020-07-21 00:55:33 -0700

    Binary Seach Simple - Recursive

--- algorithms/binary-search.c
+++ algorithms/binary-search.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #define MAX_SIZE 10
 
+int binarySearchRecursive(int * arr, int start, int end, int n) {
+    if (start == end) return -1;
+    int mid = start + (end-start)/2;
+    if (n == arr[mid]) return mid;
+    if (n < arr[mid]) return binarySearchRecursive(arr, start, mid-1, n);
+    if (n > arr[mid]) return binarySearchRecursive(arr, mid+1, end, n);
+}
+
 int binarySearch(int * arr, int end, int n) {
-    int start = 0;
-    int mid = (start+end)/2;
-    int c = 0;
+    int start = 0, mid = 0;
 
     while (start <= end) {
-        printf("%d\n", c);
+        mid = start + (end-start)/2;
         if (arr[mid] == n) return mid;
         if (n < arr[mid]) {
             end = mid-1;
@@ -16,8 +22,6 @@ int binarySearch(int * arr, int end, int n) {
         else {
             start = mid+1;
         }
-        mid = (start+end)/2;
-        c++;
     }
     return -1;
 }
@@ -25,7 +29,9 @@ int binarySearch(int * arr, int end, int n) {
 int main() {
     int arr[MAX_SIZE] = {2,3,6,8,10,12,18,25,28,30};
 
-    printf("El numero se encuentra en la posicion %d", binarySearch(arr, MAX_SIZE, 9));
+    printf("\nEl numero se encuentra en la posicion %d\n", binarySearch(arr, MAX_SIZE, 9));
+    
+    printf("\nEl numero se encuentra en la posicion %d\n", binarySearchRecursive(arr, 0, MAX_SIZE, 2));
 
     return 0;
 }
